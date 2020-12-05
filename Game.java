441b979dc59fa470ec435693d4b07b55f4082f73
hunklesj <hunklesj@mail.uc.edu>
2020-12-05 17:46:48 +0000

    Initial commit

---
+++ Game.java
@@ -0,0 +1,18 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public Class Game implements ActionListener {
+  JLabel welcome;
+
+Game (){
+  JFrame frame = new JFrame("Group XX's Trivia Game");
+  frame.setLayout(new FlowLayout());
+  frame.setSize(220,90);
+}
+}
