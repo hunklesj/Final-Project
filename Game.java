2ea446c26a4a0421044adb402b5ae6296fc262d6
hunklesj <hunklesj@mail.uc.edu>
2020-12-08 00:53:07 +0000

    mostly finished

--- Game.java
+++ Game.java
@@ -6,8 +6,9 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+
 public class Game implements ActionListener {
-  JLabel label2;
+  JLabel finish;
   JLabel scoreLabel;
   JLabel currentQuestion;
   JLabel response;
@@ -16,39 +17,43 @@ public class Game implements ActionListener {
   static String filename;
   static FileReader myFile;
   int i = 0;
-
-  Game() {
-  JFrame frame = new JFrame("Group XX's Trivia Game");
+  JLabel page;
+  int questionNumber = 1;
+  JFrame frame;
+  JButton buttonA;
+  JButton buttonB;
+  JButton buttonC;
+  JButton buttonD;
+  JButton nextQuestion;
+  JButton start;
+  
+  Game() { 
+  frame = new JFrame("Group XX's Trivia Game");
   frame.setLayout(new FlowLayout());
   frame.setSize(500,500);
 
-  currentQuestion = new JLabel (questions.get(i).getQuestion());
-  frame.add(currentQuestion);
-  JButton buttonA = new JButton (questions.get(i).getA());
-  JButton buttonB = new JButton (questions.get(i).getB());
-  JButton buttonC = new JButton (questions.get(i).getC());
-  JButton buttonD = new JButton (questions.get(i).getD());
-  JButton nextQuestion = new JButton ("Next Question");
-
-  buttonA.addActionListener(this);
-  buttonB.addActionListener(this);
-  buttonC.addActionListener(this);
-  buttonD.addActionListener(this);
+  buttonB = new JButton ("");
+  buttonC = new JButton ("");
+  buttonD = new JButton ("");
+  start = new JButton ("Start");
+  buttonA = new JButton ("");
+  nextQuestion = new JButton ("");
+  start.addActionListener(this);
+  nextQuestion.setText("Next Question");
   nextQuestion.addActionListener(this);
-  frame.add(buttonA); 
-  frame.add(buttonB);
-  frame.add(buttonC);
-  frame.add(buttonD);
+  
+  page = new JLabel("");
+  frame.add(page);
+  frame.add(start);
 
-  frame.add(nextQuestion);
-
-  label2 = new JLabel("Worth " + (questions.get(i).getPoints()));
+  currentQuestion = new JLabel ("");
+  frame.add(currentQuestion);
+  finish = new JLabel("");
   response = new JLabel ("");
-  frame.add(label2);
   scoreLabel = new JLabel ("");
   frame.add(scoreLabel);
-  frame.setVisible(true);
   frame.add(response);
+  frame.setVisible (true);
 }
 
 public void actionPerformed(ActionEvent ae) {
@@ -79,22 +84,90 @@ reader.close();
 System.out.println("An error occurred: " + exception);
 }
 
-if(ae.getActionCommand().equals("Next Question")) {
-  i++;
-}
-else if (ae.getActionCommand().equals(questions.get(i).getAnswer())) {
-  scoreTotal = scoreTotal + (questions.get(i).getPoints());
-  String s = Integer.toString(scoreTotal);
-  response.setText("Correct!");
-  scoreLabel.setText("Current Score:" + scoreTotal);
-  }
+  if (ae.getActionCommand().equals("Start")) {
+    currentQuestion.setText(questions.get(0).getQuestion());
+    buttonA.setText(questions.get(0).getA());
+    buttonB.setText(questions.get(0).getB());
+    buttonC.setText(questions.get(0).getC());
+    buttonD.setText(questions.get(0).getD());
 
-  else {
-    response.setText(questions.get(i).getAnswer());
-    i++;
-  }
+    buttonA.addActionListener (this);
+    buttonB.addActionListener(this);
+    buttonC.addActionListener(this);
+    buttonD.addActionListener(this);
+  
+
+    frame.add(buttonA);
+    frame.add(buttonB);
+    frame.add(buttonC);
+    frame.add(buttonD);
+    start.setVisible(false);
+    }
+
+    else if(ae.getActionCommand().equals(questions.get(i).getAnswer())){
+      buttonA.setVisible(false);
+      buttonB.setVisible(false);
+      buttonC.setVisible(false);
+      buttonD.setVisible(false);
+      currentQuestion.setText("");
+      frame.add(nextQuestion);
+      nextQuestion.setVisible(true);
+      response.setVisible(true);
+      response.setText("Correct!");
+      scoreTotal = scoreTotal + (questions.get(i).getPoints());
+      String s = Integer.toString(scoreTotal);
+      scoreLabel.setText(s);
+    }
+
+
+  else if (ae.getActionCommand().equals("Next Question")) {
+            if (questionNumber != questions.size()){
+            i++;
+            questionNumber++;
+            String p = Integer.toString(questionNumber);
+            page.setText(p);
+            buttonA.setVisible(true);
+            buttonB.setVisible(true);
+            buttonC.setVisible(true);
+            buttonD.setVisible(true);
+            nextQuestion.setVisible(false);
+            response.setVisible(false);
+            currentQuestion.setText(questions.get(i).getQuestion());
+            buttonA.setText(questions.get(i).getA());
+            buttonB.setText(questions.get(i).getB());
+            buttonC.setText(questions.get(i).getC());
+            buttonD.setText(questions.get(i).getD());
+    }
+            else {
+              
+                finish.setText("finished");
+                scoreLabel.setVisible(true);
+                frame.add(finish);
+                buttonA.setVisible(false);
+                buttonB.setVisible(false);
+                buttonC.setVisible(false);
+                buttonD.setVisible(false);
+                nextQuestion.setVisible(false);
+                response.setVisible(false);
+                page.setVisible(false);
+
+                }
+            }
+else {
+      buttonA.setVisible(false);
+      buttonB.setVisible(false);
+      buttonC.setVisible(false);
+      buttonD.setVisible(false);
+      frame.add(nextQuestion);
+      currentQuestion.setText("");
+      nextQuestion.setVisible(true);
+      response.setText("Incorrect!");
+      response.setVisible(true);
+      String s = Integer.toString(scoreTotal);
+      scoreLabel.setText(s);
 }
 
+}
 }
 
 
